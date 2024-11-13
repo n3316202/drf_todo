528eab806584b061cb106649d6b183847b194d6a
heo <admin@admin.com>
2024-11-13 16:21:07 +0900

    commit

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from .views import TodoAPIView, TodosAPIView
 
 urlpatterns = [
-
+    path('todo/', TodosAPIView.as_view()),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .models import Todo
+from .serializers import TodoSimpleSerializer, TodoDetailSerializer, TodoCreateSerializer
+
+class TodosAPIView(APIView):
+    
+    def get(self, request):
+        todos = Todo.objects.filter(complete=False)
+        serializer = TodoSimpleSerializer(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
